2e81fbe8436371879ca7a59d2d1df3cce45dc1a7
Alzayady <ahmedalzayady295@gmail.com>
2021-04-17 14:10:26 +0200

    update inorder to visualise back traverse from right tree

--- public/js/Tree/BinarySearchTree/InOrder.js
+++ public/js/Tree/BinarySearchTree/InOrder.js
@@ -2,39 +2,49 @@ class InOrder {
   constructor(tree) {
     this.tree = tree;
   }
-  traverse(node = this.tree.root, callBack, IS_END = true) {
+  traverse(node = this.tree.root, callBack) {
     if (node == null) return;
     var end_funtion = () => {
-      if (IS_END) {
-        this.tree.clearTree();
-        return;
+      if (node == this.tree.root) {
+        if (node.has_right()) {
+          // if root has right node  then we wait one qunatam after toggle the root back after trave the right tree
+          this.tree.controller.wait(() => {
+            this.tree.clearTree();
+          });
+        } else {
+          this.tree.clearTree();
+        }
+      } else {
+        callBack();
       }
-      callBack();
     };
-    // IS_END is still true if and only if we reach at the very right of the tree
-    // but what about middle nodes which has IS_END = true ?
-    // in this case it will continue proagate to the end and won't go through that branch calling end_function
     this.tree.controller.toggle(node, () => {
       node.removeCursor();
       if (node.has_left()) {
-        this.traverse(
-          node.get_left(),
-          () => {
-            if (node.has_right()) {
-              this.tree.controller.toggle_select(node, () => {
-                this.traverse(node.get_right(), callBack, IS_END);
+        this.traverse(node.get_left(), () => {
+          if (node.has_right()) {
+            this.tree.controller.toggle_select(node, () => {
+              this.traverse(node.get_right(), () => {
+                this.tree.controller.toggle(node, () => {
+                  node.addCursorSelected();
+                  end_funtion();
+                });
               });
-            } else {
-              this.tree.controller.toggle_select(node, () => {
-                end_funtion();
-              });
-            }
-          },
-          false
-        );
+            });
+          } else {
+            this.tree.controller.toggle_select(node, () => {
+              end_funtion();
+            });
+          }
+        });
       } else if (node.has_right()) {
         this.tree.controller.select(node, () => {
-          this.traverse(node.get_right(), callBack, IS_END);
+          this.traverse(node.get_right(), () => {
+            this.tree.controller.toggle(node, () => {
+              node.addCursorSelected();
+              end_funtion();
+            });
+          });
         });
       } else {
         this.tree.controller.select(node, () => {
