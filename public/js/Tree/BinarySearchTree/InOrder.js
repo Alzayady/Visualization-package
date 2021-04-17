440eba6122ca72c0ed39207c107a08d93ac22a45
Alzayady <ahmedalzayady295@gmail.com>
2021-04-17 14:26:01 +0200

    update inorder to visualise back traverse from right tree & claen code

--- public/js/Tree/BinarySearchTree/InOrder.js
+++ public/js/Tree/BinarySearchTree/InOrder.js
@@ -2,55 +2,56 @@ class InOrder {
   constructor(tree) {
     this.tree = tree;
   }
+
   traverse(node = this.tree.root, callBack) {
     if (node == null) return;
-    var end_funtion = () => {
-      if (node == this.tree.root) {
-        if (node.has_right()) {
-          // if root has right node  then we wait one qunatam after toggle the root back after trave the right tree
-          this.tree.controller.wait(() => {
-            this.tree.clearTree();
-          });
-        } else {
-          this.tree.clearTree();
-        }
-      } else {
-        callBack();
-      }
-    };
     this.tree.controller.toggle(node, () => {
       node.removeCursor();
       if (node.has_left()) {
-        this.traverse(node.get_left(), () => {
-          if (node.has_right()) {
-            this.tree.controller.toggle_select(node, () => {
-              this.traverse(node.get_right(), () => {
-                this.tree.controller.toggle(node, () => {
-                  node.addCursorSelected();
-                  end_funtion();
-                });
-              });
-            });
-          } else {
-            this.tree.controller.toggle_select(node, () => {
-              end_funtion();
-            });
-          }
-        });
+        this.traverse_left(node, callBack);
       } else if (node.has_right()) {
-        this.tree.controller.select(node, () => {
-          this.traverse(node.get_right(), () => {
-            this.tree.controller.toggle(node, () => {
-              node.addCursorSelected();
-              end_funtion();
-            });
-          });
-        });
+        this.traverse_right(node, callBack);
       } else {
         this.tree.controller.select(node, () => {
-          end_funtion();
+          this.end_funtion(node, callBack);
         });
       }
     });
+  }
+
+  traverse_left(node, callBack) {
+    this.traverse(node.get_left(), () => {
+      if (node.has_right()) {
+        this.traverse_right(node, callBack);
+      } else {
+        this.tree.controller.toggle_select(node, () => {
+          this.end_funtion(node, callBack);
+        });
+      }
+    });
+  }
+  traverse_right(node, callBack) {
+    this.tree.controller.toggle_select(node, () => {
+      this.traverse(node.get_right(), () => {
+        this.tree.controller.toggle(node, () => {
+          node.addCursorSelected();
+          this.end_funtion(node, callBack);
+        });
+      });
+    });
+  }
+  end_funtion(node, callBack) {
+    if (node == this.tree.root) {
+      if (node.has_right()) {
+        // if root has right node  then we wait one qunatam after toggle the root back after trave the right tree
+        this.tree.controller.wait(() => {
+          this.tree.clearTree();
+        });
+      } else {
+        this.tree.clearTree();
+      }
+    } else {
+      callBack();
+    }
   }
 }
